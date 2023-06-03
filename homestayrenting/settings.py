1bfcca838c8a51b505746adb1290436e34831f45
Viet Ngo Quang <viet.nq194881@sis.hust.edu.vn>
2023-06-03 19:49:30 +0700

    change project name to 'homestayrenting'

--- homestayrenting/settings.py
+++ homestayrenting/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'helloworld.urls'
+ROOT_URLCONF = 'homestayrenting.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'helloworld.wsgi.application'
+WSGI_APPLICATION = 'homestayrenting.wsgi.application'
 
 
 # Database
