aaed65c6707829cb64aeaf5850d1ee4dd4536c56
Nguyen Van Trinh <nguyenvantrinh2607@gmail.com>
2023-06-18 00:14:43 +0700

    hotfix: add cors header

--- homestayrenting/settings.py
+++ homestayrenting/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    "corsheaders",
 
     'users.apps.UsersConfig',
 ]
@@ -60,14 +61,17 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS: True
 
 AUTH_USER_MODEL = "users.User"
 
