149d7ff0bacf9d7aa2e1537e49e3cdd33a2787ef
Trung <trung.ct190098@sis.hust.edu.vn>
2023-07-16 12:52:21 +0700

    Pricing Config for Homestay Details

--- homestays/views.py
+++ homestays/views.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Homestay, Service
-from .serializers import HomestaySerializer, ServiceSerializer, ServiceGetSerializer
+from .serializers import HomestaySerializer, ServiceSerializer, ServiceGetSerializer, HomestayGetSerializer
 from django.db.models import Q
-from myadmin.models import ServiceType
+from myadmin.models import ServiceType, PricingConfig
 
 
 @permission_classes([IsAuthenticatedOrReadOnly])
@@ -69,8 +69,8 @@ class HomestayDetail(APIView):
                 })
         reviews.sort(key=lambda x: x['review_timestamp'], reverse=True)
         avg_rating = None if len(reviews) == 0 else sum(int(review['rating']) for review in reviews) / len(reviews)
-
-        serializer = HomestaySerializer(homestay)
+        homestay.pricing_config = PricingConfig.objects.get(pk = homestay.pricing_config_id_id)
+        serializer = HomestayGetSerializer(homestay)
         data = serializer.data
         data['reviews'] = reviews
         data['avg_rating'] = avg_rating
