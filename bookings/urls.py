e42affe83646b45c83f04025e838f12d4fab39f1
Viet Ngo Quang <viet.nq194881@sis.hust.edu.vn>
2023-07-16 14:20:31 +0700

    add booking generator, api get booked date of a homestay

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('bookings/', views.BookingList.as_view(), name='all_bookings'),
+    path('bookings/booked_dates/<homestay_id>/', views.BookedDates.as_view(), name='free_dates'),
     path('bookings/<str:username>/', views.BookingList.as_view(), name='booking'),
     path('bookings/<str:username>/<str:booking_id>/', views.BookingDetail.as_view(), name='booking_detail'),
 ]
